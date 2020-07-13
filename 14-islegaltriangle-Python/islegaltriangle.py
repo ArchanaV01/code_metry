8038d10c070eed80b406dd0740a4fed4a4a0c0a0
Archana <archana.vellanki95@msitprogram.net>
2020-07-13 21:16:25 +0530

    Jul 13  21:16:25

--- 14-islegaltriangle-Python/islegaltriangle.py
+++ 14-islegaltriangle-Python/islegaltriangle.py
@@ -10,5 +10,5 @@ def islegaltriangle(s1, s2, s3):
     # your code goes here
     if s1 <= 0 or s2 <= 0 or s3 <= 0:
         return False
-    sides = sorted[s1, s2, s3]
+    sides = sorted([s1, s2, s3])
     return sides[0] + sides[1] > sides[2]
