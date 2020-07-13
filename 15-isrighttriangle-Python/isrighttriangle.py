18f6cef57c8f0ab2715c88c8025a4a14181e66ec
Archana <archana.vellanki95@msitprogram.net>
2020-07-13 21:43:51 +0530

    Jul 13  21:43:51

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -13,4 +13,4 @@ def isrighttriangle(x1, y1, x2, y2, x3, y3):
     dist2 = math.sqrt(abs(x3-x2)**2 + abs(y3-y2)**2)
     dist3 = math.sqrt(abs(x1-x3)**2 + abs(y1-y3)**2)
     distances = sorted([dist1, dist2, dist3])
-    return distances[0] + distances[1] > distances[2] and math.isclose(distances[0]**2 + distances[1]**2, distances[2]**2, abs_tol=0.01)
+    return distances[0] + distances[1] > distances[2] and math.isclose(distances[0]**2 + distances[1]**2, distances[2]**2, rel_tol=0.0001)
