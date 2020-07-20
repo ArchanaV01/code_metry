5cb88ab20f39bd2c40028020e706499625ea7a24
Archana <archana.vellanki95@msitprogram.net>
2020-07-20 22:33:28 +0530

    Jul 20  22:33:28

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -1,8 +1,44 @@
-# A happy prime is a number that is both happy and prime. 
-# Write the function nthHappyPrime(n) which takes a non-negative integer 
+# A happy prime is a number that is both happy and prime.
+# Write the function nthHappyPrime(n) which takes a non-negative integer
 # and returns the nth happy prime number (where the 0th happy prime number is 7).
+import math
+numbs = []
 
+
+def ishappynumber(n):
+    global numbs
+    if n < 1:
+        return False
+    elif n == 1:
+        numbs = []
+        return True
+    else:
+        sumDigits = 0
+        while (n > 0):
+            sumDigits += (n % 10)**2
+            n //= 10
+        if sumDigits in numbs:
+            numbs = []
+            return False
+        else:
+            numbs.append(sumDigits)
+            return ishappynumber(sumDigits)
+
+
+def isPrime(n):
+    for i in range(2, int(math.sqrt(n))):
+        if n % i == 0:
+            return False
+    return True
 
 
 def fun_nth_happy_prime(n):
-	return 0
+    counter = 0
+    numb = 1
+    happy_numb = 0
+    while counter <= n:
+        if ishappynumber(numb) and isPrime(numb):
+            counter += 1
+            happy_numb = numb
+        numb += 1
+    return happy_numb
