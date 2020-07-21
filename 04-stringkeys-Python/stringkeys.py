b0f0bc609cf0e4b3acedcf03895d1808d8a01471
Archana <archana.vellanki95@msitprogram.net>
2020-07-22 00:39:04 +0530

    Jul 22  00:39:04

--- 04-stringkeys-Python/stringkeys.py
+++ 04-stringkeys-Python/stringkeys.py
@@ -2,6 +2,7 @@
 in a hash table, where keys are calculated
 using the first two letters of the string."""
 
+
 class HashTable(object):
     def __init__(self):
         self.table = [None]*10000
@@ -9,21 +10,37 @@ class HashTable(object):
     def store(self, string):
         """Input a string that's stored in 
         the table."""
-        # Hash Value = (ASCII Value of First Letter * 100) + ASCII Value of Second Letter 
+        # Hash Value = (ASCII Value of First Letter * 100) + ASCII Value of Second Letter
         # Your code goes here
-        pass
-        
+        hashVal = calculate_hash_value(string)
+        while self.table[hashVal] and hashVal < 10000:
+            hashVal += 1
+        if hashVal >= 10000:
+            hashVal = 0
+            while self.table[hashVal]:
+                hashVal += 1
+        if not self.table[hashVal]:
+            self.table[hashVal] = string
+
     def lookup(self, string):
         """Return the hash value if the
         string is already in the table.
         Return -1 otherwise."""
         # Your code goes here
-        pass
+        hashVal = calculate_hash_value(string)
+        while self.table[hashVal] != string and hashVal < 10000:
+            hashVal += 1
+        if hashVal >= 10000:
+            hashVal = 0
+            while self.table[hashVal] != string:
+                hashVal += 1
+        if self.table[hashVal] == string:
+            return hashVal
+        else:
+            return -1
 
     def calculate_hash_value(self, string):
         """Helper function to calulate a
         hash value from a string."""
         # Your code goes here
-        pass
-
-
+        return ord(string[0])*100 + ord(string[1])
