354eaa7c03a7af3cf7b701c5ddd85965fa4886a3
Archana <archana.vellanki95@msitprogram.net>
2020-07-22 20:47:43 +0530

    Jul 22  20:47:43

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -8,7 +8,9 @@ import math
 def isPrime(n):
     for i in range(2, int(math.sqrt(n))+1):
         if n % i == 0:
+            print(n, "not prime")
             return False
+    print(n, "prime")
     return True
 
 
