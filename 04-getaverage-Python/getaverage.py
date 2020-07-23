f4ffbdda8c7009ff27442cd597ef590f09cf1f6b
Archana <archana.vellanki95@msitprogram.net>
2020-07-23 11:21:57 +0530

    Jul 23  11:21:57

--- 04-getaverage-Python/getaverage.py
+++ 04-getaverage-Python/getaverage.py
@@ -1,14 +1,19 @@
-# Write the function getAverage(values) that takes a string of 
-# comma-separated non-negative integer values, and returns their 
-# average as a float (even though the values themselves are integers). 
-# Note that some values may not be non-negative integers, and these 
+# Write the function getAverage(values) that takes a string of
+# comma-separated non-negative integer values, and returns their
+# average as a float (even though the values themselves are integers).
+# Note that some values may not be non-negative integers, and these
 # should be ignored. If there are no integer values, return 0 (do not crash here).
-# For example, getAverage('13,excused,14,absent') ignores the two 
+# For example, getAverage('13,excused,14,absent') ignores the two
 # strings and averages 13 and 14 to return 13.5. Also, getAverage('a,b,c') returns 0.
 
 
-
-
-def fun_getaverage(s): 
-	return 0.0
-
+def fun_getaverage(s):
+    sum_numb = 0
+    count_numb = 0
+    for each in s.split(","):
+        try:
+            sum_numb += int(each)
+            count_numb += 1
+        except TypeError:
+            continue
+    return sum_numb/count_numb
